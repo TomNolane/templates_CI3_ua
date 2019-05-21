7db26474311ea20bff5b4b9a6e452daf077bbc46
xzeno <34212539+xzeno@users.noreply.github.com>
2019-05-21 13:44:39 +0300

    update zaimomirsu

---
+++ zaimomirsu/header_offerwall.php
@@ -0,0 +1,93 @@
+<?php 
+//require 'templates/common/new2/php/check_bot.php';
+if(!isset($my_title)) 
+	$my_title = 'Срочные займы круглосуточно без проверок Онлайн';
+
+if(!isset($description))
+	$description = 'Zaimomir - лучший онлайн сервис по выдаче мгновенных займов и кредитов без проверки вашей кредитной истории. Только у нас лучшие кредитные предложения!';
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title><?php echo $my_title;?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="Description" content="<?php echo $description;?>" />
+    <meta name="Keywords" content="моментальный заем, займы, онлайн, деньги" />
+    <meta name="robots" content="all" />
+    <meta name="copyright" lang="ru" content="zaimomir.su" /> 
+    <meta property="og:title" content="Срочные займы круглосуточно без проверок Онлайн" />
+    <meta property="og:description" content="Zaimomir - лучший онлайн сервис по выдаче мгновенных займов и кредитов без проверки вашей кредитной истории.Только у нас лучшие кредитные предложения!"/>
+    <meta property="og:image" content="https://zaimomir.su/templates/zaimomirsu/assets/img/creditcard.png"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="https://zaimomir.su/" />
+    <meta name="twitter:card" content="https://zaimomir.su/templates/zaimomirsu/assets/img/creditcard.png"/>
+    <meta name="twitter:title" content="Срочные займы круглосуточно без проверок Онлайн"/>
+    <meta name="twitter:description" content="Zaimomir - лучший онлайн сервис по выдаче мгновенных займов и кредитов без проверки вашей кредитной истории.Только у нас лучшие кредитные предложения!"/>
+    <meta name="twitter:image:src" content="https://zaimomir.su/templates/zaimomirsu/assets/img/creditcard.png"/>
+    <meta name="twitter:url" content="https://zaimomir.su/"/>
+    <meta name="twitter:domain" content="zaimomir.su"/>
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="/templates/zaimomirsu/favicon/favicon-16x16.png" type="image/png">
+    <link rel="apple-touch-icon" sizes="57x57" href="/templates/zaimomirsu/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/templates/zaimomirsu/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/templates/zaimomirsu/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/templates/zaimomirsu/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/templates/zaimomirsu/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/templates/zaimomirsu/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/templates/zaimomirsu/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/templates/zaimomirsu/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/templates/zaimomirsu/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="/templates/zaimomirsu/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/templates/zaimomirsu/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/templates/zaimomirsu/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/templates/zaimomirsu/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/templates/zaimomirsu/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/templates/zaimomirsu/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff"> 
+    <!-- Styles -->
+    <link rel="stylesheet" href="/templates/zaimomirsu/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/templates/zaimomirsu/assets/css/pixelstyle.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <script src="//cdn.sendpulse.com/js/push/2e801fea168aa576cb7129e36291c8e6_1.js" async></script>
+</head>
+
+<body>
+
+<!-- header -->
+<header data-block-type="headers" class="">
+    <div class="container">
+        <nav class="navbar navbar-expand-md">
+            <a class="navbar-brand" href="/">
+                <img src="/templates/zaimomirsu/assets/img/logo.png" height="30" alt="image">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav3" aria-controls="navbarNav3" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav3">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Документы</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdown01">
+                            <a class="dropdown-item" href="/oferta">Публичная оферта</a>
+                            <a class="dropdown-item" href="/soglasie">Согласие на обработку данных</a>
+                            <a class="dropdown-item" href="/rules">Правила предоставления займов</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">О сервисе</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/faq">FAQ</a>
+                    </li>
+                </ul>
+
+                <button class="btn btn-outline-primary ml-md-3 modal-contact" data-toggle="modal" data-target="#askQuestion" href="#">Обратная связь</button>
+            </div>
+        </nav>
+    </div>
+</header>
+<!-- /header -->
