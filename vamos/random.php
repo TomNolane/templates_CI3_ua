c4aab2cd84e17c80b46e95b0e1d079311d5da184
SlaSh <slashka100500@gmail.com>
2019-05-23 15:55:58 +0300

    random BG ONLY 4 zaimnow.su

---
+++ vamos/random.php
@@ -0,0 +1,189 @@
+<?
+class Random {
+    const PARAMS_FILEPATH = 'templates/vamos/random/random.json';
+    const BG_PATH = 'templates/vamos/img/universal';
+    const RNDENTITY = 'templates/vamos/random/img';
+
+    private $params;
+    private $useSummFormat;
+    private $buttonTextInUpperCase;
+    private $usePath;
+
+    private $lastBgColors;
+
+    private $gradienDirectionsStart = array('left', 'top', '-45deg', '45deg');
+    private $gradienDirectionsEnd = array('to right', 'to bottom', '135deg', '45deg');
+
+    public function __construct ($useSummFormat = true, $buttonTextInUpperCase = true, $usePath = true) {
+        $this->useSummFormat = $useSummFormat;
+        $this->buttonTextInUpperCase = $buttonTextInUpperCase;
+        $this->usePath = $usePath;
+
+        $this->loadParams();
+    }
+
+    private function loadParams() {
+        $str = file_get_contents(self::PARAMS_FILEPATH);
+        $this->params = json_decode($str);
+    }
+
+    private function getHexByColor ($color) {
+        for ($i = 0; $i < count($this->params->hexesforcolors); $i++) {
+            if ($color == $this->params->hexesforcolors[$i]->color)
+                return $this->params->hexesforcolors[$i]->hex;
+        }
+
+        $default = 'yellow';
+        for ($i = 0; $i < count($this->params->hexesforcolors); $i++) {
+            if ($default == $this->params->hexesforcolors[$i]->color)
+                return $this->params->hexesforcolors[$i]->hex;
+        }
+    }
+
+    /**
+     * получаем два случайных цвета, которые не пересекаются с цветами бекграунда
+     */
+    public function getPreferredColors() {
+        $arr = array();
+        $bw = array('white', 'black');
+        if (in_array($bw, $this->lastBgColors))
+            $arr = array_diff($bw, $this->lastBgColors);
+        else $arr = array_diff($this->params->availablecolors, $this->lastBgColors, $bw, array('gray'));
+
+        $r = rand(0, count($arr) - 1);
+        $r2 = $r;
+        while ($r2 == $r) $r2 = rand(0, count($arr) - 1);
+
+        //restore indexes
+        $arr_fixed = array();
+        foreach ($arr as $key => $val) $arr_fixed[] = $val;
+
+        return array($this->getHexByColor($arr_fixed[$r]), $this->getHexByColor($arr_fixed[$r2]));
+    }
+
+    /**
+     * получить случайный бекграунд
+     */
+    public function getBackground() {
+        $arr = array();
+        for ($i = 0; $i < count($this->params->background); $i++) {
+            if (!isset($this->params->background[$i]->inuse)) continue;
+            if ($this->params->background[$i]->inuse == 1) $arr[] = $i;
+        }
+        $num = count($arr);
+        $r = rand(0, $num - 1);
+
+        $this->lastBgColors = $this->params->background[$arr[$r]]->color;
+
+        return (($this->usePath)? ((isset($this->params->background[$arr[$r]]->rndentity))? self::RNDENTITY: self::BG_PATH) . '/': '') . $this->params->background[$arr[$r]]->style->background;
+    }
+
+    /**
+     * получить стиль случайного градиента без цветов, переданных в функцию
+     */
+    public function getGradientBackground ($exclude_colors = array()) {
+        $r = null;
+        while ($r === null) {
+            $r = $this->params->gradients[rand(0, count($this->params->gradients) - 1)];
+            if (in_array($r->color, $exclude_colors))
+                $r = null;
+        }
+        $r2 = $r;
+        while ($r2->hex == $r->hex) {
+            $r2 = $this->params->gradients[rand(0, count($this->params->gradients) - 1)];
+            if (in_array($r2->color, $exclude_colors))
+                $r2 = $r;
+        }
+        $r1h = $r->hex;
+        $r2h = $r2->hex;
+
+        $this->lastBgColors = array($r->color, $r2->color);
+
+        $direction = rand(0, count($this->gradienDirectionsStart) - 1);
+        $dstart = $this->gradienDirectionsStart[$direction];
+        $dend = $this->gradienDirectionsEnd[$direction];
+
+        $out = "
+        background: $r1h;
+        background: -moz-linear-gradient($dstart, $r1h 0%, $r2h 100%);
+        background: -webkit-linear-gradient($dstart, $r1h 0%,$r2h 100%);
+        background: linear-gradient($dend, $r1h 0%,$r2h 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='$r1h', endColorstr='$r2h',GradientType=0 );";
+
+        return $out;
+    }
+    //TODO: зафигачить принт класса, дабы не инлайн. Хотя, в шаблоне все инлайн...
+
+    /**
+     * получить случайную сумму
+     */
+    private function getSumm() {
+        $num = count($this->params->summ);
+        $r = rand(0, $num - 1);
+        return $this->params->summ[$r];
+    }
+
+    /**
+     * получить случайный текст
+     */
+    public function getText() {
+        $summ = $this->getSumm();
+        if ($this->useSummFormat) $summ = number_format($summ, 0, '', ' ');
+
+        $arr = array();
+
+        for ($i = 0; $i < count($this->params->text->values); $i++) {
+            if (!isset($this->params->text->values[$i]->inuse)) continue;
+            if ($this->params->text->values[$i]->inuse == 1) $arr[] = $i;
+        }
+        $num = count($arr);
+        $r = rand(0, $num - 1);
+        $txt = $this->params->text->values[$arr[$r]];
+        $symbols = (isset($txt->summ_symbol))? $txt->summ_symbol: $this->params->text->summ_symbol;
+
+        return str_replace($symbols, $summ, $txt->text);
+    }
+
+    /**
+     * получить случайное количество слов для анимированного текста
+     */
+    public function getAnimatedTextRaw ($count = 5) {
+        $arr = array();
+        if (count($this->params->animatedtext) < $count)
+            $count = count($this->params->animatedtext);
+        
+        while (count($arr) != $count) {
+            while (true) {
+                $txt = $this->params->animatedtext[rand(0, $count - 1)];
+                if (!in_array($txt, $arr)) {
+                    $arr[] = $txt;
+                    break;
+                }
+            }
+        }
+
+        return $arr;
+    }
+
+    /**
+     * хелпер для анимированного текста
+     */
+    public function printAnimatedText ($count = 5) {
+        $arr = $this->getAnimatedTextRaw ($count);
+        $out = '';
+
+        for ($i = 0; $i < count($arr); $i++)
+            $out .= 'data-text' . ($i + 1) . '="' . $arr[$i] . '" ';
+        
+        return $out;
+    }
+
+    /**
+     * случайный текст на кнопку
+     */
+    public function getButtonText() {
+        $r = rand(0, count($this->params->button->text) - 1);
+        return ($this->buttonTextInUpperCase)? mb_strtoupper($this->params->button->text[$r], 'utf8'): $this->params->button->text[$r];
+    }
+}
+?>
--- vamos/universal.php
+++ vamos/universal.php
@@ -8,7 +8,7 @@
         $link = str_replace("#name#", implode('',$this->input->get()), $link);
     else 
         $link = str_replace("#name#", $domen, $link);
-    require 'templates/common/new/php/check_bot.php';
+    require 'templates/common/new2/php/check_bot.php';
 
     function getDomainUrl()
     {
@@ -20,8 +20,18 @@
     $maindomain = getDomainUrl();
     $email = "info@".$maindomain;
 
-    $my_title = getDomainUrl().$my_title; 
-    $description = getDomainUrl().$description;
+    $random = null;
+    if (in_array($maindomain, array('zaimnow.su', 'zaimnowsu.tomnolane.ru'))) {
+        require 'random.php';
+        $random = new Random();
+    }
+    // random backs go here
+    if ($random !== null) {
+        $rnd_bg = $random->getBackground();
+        $preferredColors = $random->getPreferredColors();
+        $gradient_bg = $random->getGradientBackground();
+        $preferredGradientColors = $random->getPreferredColors();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -34,7 +44,11 @@
     <meta property="og:title" content="Вам одобрено!">
     <meta property="og:description" content="">
     <meta property="og:type" content="website">
-    <meta property="og:image" content="/templates/vamos/img/universal/<?=$main?>_header.jpg">
+    <?php if ($random !== null) : ?>
+        <meta property="og:image" content="/<?=$rnd_bg?>">
+    <?php else : ?>
+        <meta property="og:image" content="/templates/vamos/img/universal/<?=$main?>_header.jpg">
+    <?php endif; ?>
     <meta name="format-detection" content="telephone=no">
     <link rel="canonical" href="https://<?=$maindomain?>/<?=$main?>">
     <link rel="shortcut icon" href="/templates/vamos/img/universal/<?=$main?>.ico" type="image/x-icon">
@@ -134,7 +148,7 @@
     }
 }
 <?php
-require 'templates/common/new/css/common.css';
+require 'templates/common/new2/css/common.css';
 ?>
 li > a {
     font-size: 16px;
@@ -240,11 +254,19 @@ li > a {
         <div id="rec63469314" class="r t-rec t-screenmin-480px" style=" " data-animationappear="off" data-record-type="338"
             data-screen-min="480px">
             <!-- cover -->
-            <div class="t-cover" id="recorddiv63469314"  style="height:100vh; background-image:url(&#39;/templates/vamos/img/universal/<?=$main?>_header.jpg&#39;);">
+            <?php if ($random !== null) : ?>
+            <div class="t-cover" id="recorddiv63469314"  style="height:100vh; background-image:url(&#39;/<?=$rnd_bg?>&#39;);">
                 <div class="t-cover__carrier loaded" id="coverCarry63469314" data-content-cover-id="63469314"
-                    data-content-cover-bg="/templates/vamos/img/universal/<?=$main?>_header.jpg"
-                    data-content-cover-height="100vh" data-content-cover-parallax="" style="height: 100vh; background-attachment: scroll; background-image: url(&quot;/templates/vamos/img/universal/<?=$main?>_header.jpg&quot;);"
+                    data-content-cover-bg="/<?=$rnd_bg?>"
+                    data-content-cover-height="100vh" data-content-cover-parallax="" style="height: 100vh; background-attachment: scroll; background-image: url(&quot;/<?=$rnd_bg?>&quot;);"
                     ></div>
+            <?php else : ?>
+                <div class="t-cover" id="recorddiv63469314"  style="height:100vh; background-image:url(&#39;/templates/vamos/img/universal/<?=$main?>_header.jpg&#39;);">
+                    <div class="t-cover__carrier loaded" id="coverCarry63469314" data-content-cover-id="63469314"
+                        data-content-cover-bg="/templates/vamos/img/universal/<?=$main?>_header.jpg"
+                        data-content-cover-height="100vh" data-content-cover-parallax="" style="height: 100vh; background-attachment: scroll; background-image: url(&quot;/templates/vamos/img/universal/<?=$main?>_header.jpg&quot;);"
+                        ></div>
+            <?php endif; ?>
                 <div class="t-cover__filter" style="height:100vh;background-image: -moz-linear-gradient(to top, rgba(0,0,0,0.0), rgba(0,0,0,0.0));background-image: -webkit-linear-gradient(to top, rgba(0,0,0,0.0), rgba(0,0,0,0.0));background-image: -o-linear-gradient(to top, rgba(0,0,0,0.0), rgba(0,0,0,0.0));background-image: -ms-linear-gradient(to top, rgba(0,0,0,0.0), rgba(0,0,0,0.0));background-image: linear-gradient(to top, rgba(0,0,0,0.0), rgba(0,0,0,0.0));filter: progid:DXImageTransform.Microsoft.gradient(startColorStr=&#39;#fe000000&#39;, endColorstr=&#39;#fe000000&#39;);"></div>
                 <div class="t338">
                     <div class="t-container">
@@ -257,16 +279,35 @@ li > a {
                                                     data-tu-max-height="800" style="max-width:250px;" src="/templates/vamos/img/universal/<?=$main?>_logo.png"
                                                     data-original="/templates/vamos/img/universal/<?=$main?>_logo.png"
                                                      data-hook-clogo="coverlogo"></a>
-                                              <div class="t338__title t-title t-title_xxs" style="" >
-                                                <div style="text-align: center;font-size:52px;text-shadow: 1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000" data-customstyle="yes">Вам одобрено 4000 гривен после заявки.<br> Жмите получить деньги.<br>
+                                                     <?php if ($random !== null) : ?>
+                                                        <div class="t338__title t-title t-title_xxs" style="color: <?=$preferredColors[0]?> !important;">
+                                                    <?php else : ?>
+                                                        <div class="t338__title t-title t-title_xxs" style="">
+                                                    <?php endif; ?>
+                                                <div style="text-align: center;font-size:52px;text-shadow: 1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000" data-customstyle="yes">
+                                                    <?php if ($random !== null) : ?>
+                                                        <?=$random->getText();?>
+                                                        <br>
+                                                        <style>
+                                                            .t635__typing-text {color: <?=$preferredColors[1]?> !important;}
+                                                            .typed-cursor {color: <?=$preferredColors[1]?> !important;}
+                                                        </style>
+                                                        <span class="typed-cursor" style="color: <?=$preferredColors[1]?> !important;">|</span><br></div>
+                                                    <?php else : ?>
+                                                        Вам одобрено 4000 гривен после заявки.<br> Жмите получить деньги.<br>
                                                         <span class="typed-cursor">|</span><br></div>
+                                                    <?php endif; ?>
                                             </div>  
                                             <div class="t338__buttonwrapper click_offer" style="text-align: center;"><a href="<?=$link?>"
                                                     target="_blank" class="t338__btn t-btn pulse2 ex-main-btn" style="border: 1px solid;color:#000000;background-color:#21bf16;border-radius:300px; -moz-border-radius:300px; -webkit-border-radius:300px;font-weight:700;">
                                                     <table style="width:100%; height:100%;">
                                                         <tbody>
                                                             <tr>
-                                                                <td>ПОЛУЧИТЬ ДЕНЬГИ</td>
+                                                                <?php if ($random !== null) : ?>
+                                                                    <td><?=$random->getButtonText();?></td>
+                                                                <?php else : ?>
+                                                                    <td>ПОЛУЧИТЬ ДЕНЬГИ</td>
+                                                                <?php endif; ?>
                                                             </tr>
                                                         </tbody>
                                                     </table>
@@ -289,7 +330,11 @@ li > a {
         <div id="rec63581518" class="r t-rec t-screenmax-480px" style=" " data-animationappear="off" data-record-type="338"
             data-screen-max="480px">
             <!-- cover -->
-            <div class="t-cover" id="recorddiv63581518"  style="height:100vh; background-image:url(&#39;/templates/vamos/img/universal/<?=$main?>_header.jpg&#39;);">
+            <?php if ($random !== null) : ?>
+                <div class="t-cover" id="recorddiv63581518" style="height:100vh; <?=$gradient_bg?>">
+            <?php else : ?>
+                <div class="t-cover" id="recorddiv63581518"  style="height:100vh; background-image:url(&#39;/templates/vamos/img/universal/<?=$main?>_header.jpg&#39;);">
+            <?php endif; ?>
                 <!-- <div class="t-cover__carrier" id="coverCarry63581518" data-content-cover-id="63581518"
                     data-content-cover-bg="/templates/vamos/img/universal/<?=$main?>.jpg"
                     data-content-cover-height="100vh" data-content-cover-parallax="" style="height:100vh;background-attachment:scroll; "></div> -->
@@ -310,7 +355,17 @@ li > a {
                                                 data-animate-style="fadeinleft" data-animate-group="yes" style="transition-delay: 0.5s;"
                                                 >
                                                 
-                                                <div style="font-size:52px;" data-customstyle="yes"><span style="font-size: 33px;text-shadow:1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000"><br>Вам одобрено 4000 гривен после заявки. <br>Жмите получить деньги.</span><br><span style="font-size: 42px;">
+                                                <div style="font-size:52px;" data-customstyle="yes">
+                                                    <?php if ($random !== null) : ?>
+                                                        <span style="color: <?=$preferredGradientColors[0]?>; font-size: 33px;text-shadow:1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000">
+                                                            <br><?=$random->getText();?>
+                                                        </span>
+                                                    <?php else : ?>
+                                                        <span style="font-size: 33px;text-shadow:1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000">
+                                                            <br>Вам одобрено 4000 гривен после заявки. <br>Жмите получить деньги.
+                                                        </span>
+                                                    <?php endif; ?>
+                                                <br><span style="font-size: 42px;">
                                                 <!-- <span style="color: rgb(255, 226, 0);text-shadow:1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000,1px 1px 1px #000, 1px 1px 1px #000" data-redactor-style="color: rgb(255, 226, 0);">Круглосуточно. Быстро. Выгодно</span> -->
                                                 </span></div>
                                                             
@@ -321,7 +376,11 @@ li > a {
                                                     <table style="width:100%; height:100%;">
                                                         <tbody>
                                                             <tr>
-                                                                <td>ПОЛУЧИТЬ ДЕНЬГИ</td>
+                                                                <?php if ($random !== null) : ?>
+                                                                    <td><?=$random->getButtonText();?></td>
+                                                                <?php else : ?>
+                                                                    <td>ПОЛУЧИТЬ ДЕНЬГИ</td>
+                                                                <?php endif; ?>
                                                             </tr>
                                                         </tbody>
                                                     </table>
@@ -339,8 +398,12 @@ li > a {
 
         <div id="rec63469593" class="r t-rec" style=" " data-animationappear="off" data-record-type="635">
             <!-- T635 -->
-            <div class="t635__textholder" data-recid="63469314" data-text1="Круглосуточно" data-text2="Повсюду"
-                data-text3="Быстро" data-text4="Безопасно" data-text5="Одобрение 95%"></div>
+            <?php if ($random !== null) : ?>
+                <div class="t635__textholder" data-recid="63469314" <?=$random->printAnimatedText(5)?>></div>
+            <?php else : ?>
+                <div class="t635__textholder" data-recid="63469314" data-text1="Круглосуточно" data-text2="Повсюду"
+                    data-text3="Быстро" data-text4="Безопасно" data-text5="Одобрение 95%"></div>
+            <?php endif; ?>
             <script >
                 $(document).ready(function () {
                     setTimeout(function () {
@@ -691,7 +754,7 @@ li > a {
 </div>
     <script>
     <?php
-        require 'templates/common/new/js/bootstrap.min.js';
+        require 'templates/common/new2/js/bootstrap.min.js';
         $phone = '';
         if($this->uri->segment(1) && $this->uri->segment(2))
         {
