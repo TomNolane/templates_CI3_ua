4d3916030d3bbf0fa329e701ba15418f3ac6fb4e
SlaSh <slashka100500@gmail.com>
2019-05-23 16:39:55 +0300

    fix new2->new vamos universal

--- vamos/universal.php
+++ vamos/universal.php
@@ -8,7 +8,7 @@
         $link = str_replace("#name#", implode('',$this->input->get()), $link);
     else 
         $link = str_replace("#name#", $domen, $link);
-    require 'templates/common/new2/php/check_bot.php';
+    require 'templates/common/new/php/check_bot.php';
 
     function getDomainUrl()
     {
@@ -19,6 +19,9 @@
 
     $maindomain = getDomainUrl();
     $email = "info@".$maindomain;
+
+    $my_title = getDomainUrl().$my_title; 
+    $description = getDomainUrl().$description;
 
     $random = null;
     if (in_array($maindomain, array('zaimnow.su', 'zaimnowsu.tomnolane.ru'))) {
@@ -148,7 +151,7 @@
     }
 }
 <?php
-require 'templates/common/new2/css/common.css';
+require 'templates/common/new/css/common.css';
 ?>
 li > a {
     font-size: 16px;
@@ -754,7 +757,7 @@ li > a {
 </div>
     <script>
     <?php
-        require 'templates/common/new2/js/bootstrap.min.js';
+        require 'templates/common/new/js/bootstrap.min.js';
         $phone = '';
         if($this->uri->segment(1) && $this->uri->segment(2))
         {
